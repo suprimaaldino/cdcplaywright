ef75a23b3463d5b94e428ec818e1a550665b8511
Aldino Suprima <aldino.s@k2fly.com>
2025-07-02 09:06:54 +0700

    adding console script to login

--- tests/login/loginWithLockedOutUser.spec.ts
+++ tests/login/loginWithLockedOutUser.spec.ts
@@ -11,3 +11,15 @@ test('LoginWithLockedOutUser', async ({ page }) => {
   );
   await loginPage.expectLockedOutError();
 });
+
+test.afterEach(async ({ page }, testInfo) => {
+    console.log(`Finished ${testInfo.title} with status ${testInfo.status}`);
+
+    if (testInfo.status !== testInfo.expectedStatus) {
+        console.log(`Did not run as expected, ended up at ${page.url()}`);
+    }
+});
+
+test.afterAll(async () => {
+    console.log('All loginWithLockedOutUser tests have completed.');
+});
