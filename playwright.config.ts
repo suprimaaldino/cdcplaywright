e99293eaa994ca01852d1dd38c7b94a245c3db54
Aldino Suprima <aldino.s@k2fly.com>
2025-07-02 13:40:45 +0700

    update config and adding yml to integrate with gh action

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,14 +21,21 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['html', { 
+      outputFolder: process.env.TEST_SHARD 
+        ? `playwright-report/shard-${process.env.TEST_SHARD}`
+        : 'playwright-report'
+    }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     //video:'on',
     //launchOptions:{slowMo: 1000},
-     // Slow down operations by 1 second to see the actions in the video
+    // Slow down operations by 1 second to see the actions in the video
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
@@ -54,14 +61,14 @@ export default defineConfig({
     },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
 
     /* Test against branded browsers. */
     // {
