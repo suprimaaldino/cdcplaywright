b09a7b1a3aa0fa9511c67f97c55a52e2b7eb2b05
Aldino Suprima <aldino.s@k2fly.com>
2025-07-02 09:06:04 +0700

    Update cart tests and package.json configuration

--- tests/cart/addRemoveItemToCart.spec.ts
+++ tests/cart/addRemoveItemToCart.spec.ts
@@ -44,3 +44,15 @@ test.describe('cartManagementTests @reg', () => {
         });
     });
 });
+
+test.afterEach(async ({ page }, testInfo) => {
+    console.log(`Finished ${testInfo.title} with status ${testInfo.status}`);
+
+    if (testInfo.status !== testInfo.expectedStatus) {
+        console.log(`Did not run as expected, ended up at ${page.url()}`);
+    }
+});
+
+test.afterAll(async () => {
+    console.log('All addRemoveItemToCart tests have completed.');
+});
--- tests/cart/addRemoveMultipleItemsToCart.spec.ts
+++ tests/cart/addRemoveMultipleItemsToCart.spec.ts
@@ -83,3 +83,15 @@ test.describe('cartManagementTests @reg', () => {
         });
     });
 });
+
+test.afterEach(async ({ page }, testInfo) => {
+    console.log(`Finished ${testInfo.title} with status ${testInfo.status}`);
+
+    if (testInfo.status !== testInfo.expectedStatus) {
+        console.log(`Did not run as expected, ended up at ${page.url()}`);
+    }
+});
+
+test.afterAll(async () => {
+    console.log('All addRemoveMultipleItemsToCart tests have completed.');
+});
